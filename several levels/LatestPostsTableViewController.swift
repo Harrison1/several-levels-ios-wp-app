e6ccbe02955ba3e7a8dadbb5bc45227cfad0106b
Harrison McGuire <hmcguire13@gmail.com>
2016-06-07 07:48:44 -0700

    new post file

---
+++ several levels/LatestPostsTableViewController.swift
@@ -0,0 +1,24 @@
+//
+//  LatestPostsTableViewController.swift
+//  several levels
+//
+//  Created by Harrison McGuire on 6/7/16.
+//  Copyright © 2016 severallevels. All rights reserved.
+//
+
+import UIKit
+
+class LatestPostsTableViewController: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
